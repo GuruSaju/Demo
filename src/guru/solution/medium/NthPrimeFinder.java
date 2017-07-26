0f5ca5ce20c080204ad476e58fe8f47a97c1266a
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2017-07-26 12:51:26 -0400

    "Nth prime finder done"

--- src/guru/solution/medium/NthPrimeFinder.java
+++ src/guru/solution/medium/NthPrimeFinder.java
@@ -1,5 +1,7 @@
 package guru.solution.medium;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author Srisarguru
@@ -8,5 +10,38 @@ package guru.solution.medium;
  * 16th prime number. 
  */
 public class NthPrimeFinder {
-
-}
+	  public static int PrimeMover(int num) { 
+		     int max=Integer.MAX_VALUE;
+		    int count=0;
+		    int resultPrime=0;
+		    for(int i=2;i<=max;i++){
+		        if(i==2 || i==3){
+		            count=count+1;
+		        }
+		        else{
+		            int half=i/2;
+		            boolean prime=true;
+		            for(int j=2;j<=half;j++){
+		                if(i%j==0){
+		                    prime=false;
+		                }
+		            
+		            }
+		            if(prime){
+		                count++;
+		            }
+		        }
+		        if(count==num){
+		            resultPrime=i;
+		            break;
+		        }
+		    }
+		    return resultPrime;
+		  } 
+		  
+		  public static void main (String[] args) {      
+		    Scanner s = new Scanner(System.in);
+		    System.out.print(PrimeMover(Integer.parseInt(s.nextLine()))); 
+		  }   
+		  
+		}
